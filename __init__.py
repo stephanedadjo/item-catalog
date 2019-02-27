a697d01c5374c40c5bab4b051aee46174e4827aa
Dadjo Kpekouma <kdadjo@yahoo.com>
2019-02-26 22:26:14 -0500

    renamed to init

--- __init__.py
+++ __init__.py
@@ -548,5 +548,5 @@ def disconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
 
